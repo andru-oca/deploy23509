0988ddc1339b2fe699b8b04de934198cc851e7c0
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-29 20:55:01 +0000

    collect static

--- tienda_project/settings.py
+++ tienda_project/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+STACTIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
