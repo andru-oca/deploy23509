a66993f03b2ed04ca83ddd2b414e945afab23af6
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-29 21:07:50 +0000

    db

--- tienda_project/settings.py
+++ tienda_project/settings.py
@@ -73,11 +73,24 @@ WSGI_APPLICATION = "tienda_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'anderson23509$tienda_project',
+            'USER': 'anderson23509',
+            'PASSWORD': 'pirulo123',
+            'HOST': 'anderson23509.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
 
 
