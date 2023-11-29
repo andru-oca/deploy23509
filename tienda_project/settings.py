ac89aa9716b02c5dcb76092ee21d944223f0790e
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-29 21:22:23 +0000

    cors

--- tienda_project/settings.py
+++ tienda_project/settings.py
@@ -53,9 +53,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "tienda_app.cors.CorsMiddlewareMixin"
 ]
-
-MIDDLEWARE += CUSTOM_MIDDLEWARES
 
 
 ROOT_URLCONF = "tienda_project.urls"
